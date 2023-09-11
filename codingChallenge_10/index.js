46f58a47d26bbf9a818dd272665870ab89a8048b
Phạm Thanh Sơn <84237256+sonpham28052002@users.noreply.github.com>
2023-09-11 15:34:12 +0700

    add folder codingChallenge_10

---
+++ codingChallenge_10/index.js
@@ -0,0 +1,12 @@
+var again = document.querySelector(".again");
+var interval= setInterval(function () {
+    again.style.marginLeft = Math.floor(Math.random() * 1000) + "px";
+    again.style.marginTop = Math.floor(Math.random() * 1000) + "px";
+    again.style.width = Math.floor(Math.random() * 1000) + "px";
+  }, 1000);
+
+
+again.addEventListener("click", ()=>{
+    again.style.width = "15rem";
+    clearInterval(interval);
+});
